1f22607c595d308dd223653aaee4b20d86241156
Pieter Germishuys <pieter@pieterg.com>
2022-12-02 13:16:25 +0100

    Start Day 2

---
+++ 2022/rust/day2/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    #[test]
+    fn day2_part1() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+
+    #[test]
+    fn day2_part2() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+}

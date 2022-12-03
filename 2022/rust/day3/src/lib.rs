8e4fa8752576832cbad51b42176471fc6edb68ce
Pieter <pieter@pieterg.com>
2022-12-03 11:27:07 +0100

    Start Day 3

---
+++ 2022/rust/day3/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    #[test]
+    fn day3_part1() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+
+    #[test]
+    fn day3_part2() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+}

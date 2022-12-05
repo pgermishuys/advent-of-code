97303aaef8a2c538a1568aa99cb08824b5708481
Pieter Germishuys <pieter@pieterg.com>
2022-12-05 11:52:26 +0100

    Start Day 5

---
+++ 2022/rust/day5/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use std::{fs, collections::HashSet};
+
+    #[test]
+    fn day5_part1() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+
+    #[test]
+    fn day5_part2() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+}

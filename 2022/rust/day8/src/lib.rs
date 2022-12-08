9e45d65215b204ca3f3dbeaaf69a4053a015123e
Pieter Germishuys <pieter@pieterg.com>
2022-12-08 20:40:27 +0100

    Start Day 8

---
+++ 2022/rust/day8/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use std::{fs, collections::HashSet, vec::Vec};
+    use substring::Substring;
+
+    #[test]
+    fn day8_part1() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+
+    #[test]
+    fn day8_part2() {
+        let contents =
+            fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    }
+}
